c15418bd746296bf2ad44308896633b0a20404ce
Gábor Csárdi <csardi.gabor@gmail.com>
2017-04-28 10:05:08 +0100

    Fix rule test case

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -2,7 +2,7 @@
 context("printing")
 
 test_that("rule", {
-  expect_output(
+  expect_match(
     withr::with_options(list(width = 20), rule("test", pad = "-")),
     "- test -----------",
     fixed = TRUE
